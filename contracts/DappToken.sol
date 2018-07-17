9f6ddf469325ed20320cec379663981fcf5d5575
Sakander Tamoor <sikander.tamoor@gmail.com>
2018-07-17 16:58:41 +0500

    First commit

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -4,11 +4,17 @@ contract DappToken {
     //Constructor
     //Set the total number of tokens
     //Read the total number of tokens
+    //Name
+    string public name = "SIKU Token";
+    string public symbol = "SIKU";
+    string public standard = "SIKU Token v1.0";
 
     uint256 public totalSupply;
+    mapping (address => uint256) public balance0f;
 
-    function DappToken() public{
-        totalSupply = 1000000;
+    function DappToken(uint256 _initialSupply) public{
+        balance0f[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
 
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,14 +1,27 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract ('DappToken', function(accounts){
-
-    it('Sets the total Supply upon Deployment', function(){
+    it('initializes the contract with the correct values', function(){
+        return DappToken.deployed().then(function (instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal (name, 'SIKU Token','has the correct name');
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol,'SIKU','has the correct Symbol');
+        });
+    });
+    it('allocate the initial Supply upon Deployment', function(){
         return DappToken.deployed().then(function (instance) {
           tokenInstance = instance;
           return tokenInstance.totalSupply();  
         }).then(function(totalSupply){
             assert.equal(totalSupply.toNumber(),1000000,'sets the total supply to 1,000,000');
-        })
+            return tokenInstance.balance0f(accounts[0]);
+        }).then(function(adminBalance){
+             assert.equal(adminBalance.toNumber(),1000000,'it allocates the initial supply to the admin account');
+        });
     });
 
 });
