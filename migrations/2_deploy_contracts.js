4af77d6bfb9a0a156442a67f760cd21ffe37d1bd
Sakander Tamoor <sikander.tamoor@gmail.com>
2018-07-18 12:57:28 +0500

    Sale Token Add

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var DappToken = artifacts.require("./DappToken.sol");
+var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(DappToken, 1000000);
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
 };
